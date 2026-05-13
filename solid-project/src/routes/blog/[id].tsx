b3f2657939f6f686e41fbfd0ab64c49288394e24
knownkreatives <65620817+knownkreatives@users.noreply.github.com>
2026-05-13 17:18:18 +0100

    Remove dark mode styles, delete Counter component, and update About and Home pages; add Blog and Article routes

---
+++ solid-project/src/routes/blog/[id].tsx
@@ -0,0 +1,10 @@
+import { useParams } from "@solidjs/router";
+
+export default function Article() {
+  const params = useParams();
+  return (
+    <main class="text-center mx-auto text-gray-700 p-4">
+      <h1 class="max-6-xs text-6xl text-sky-700 font-thin uppercase my-16">Article {params.id}!</h1>
+    </main>
+  );
+}
